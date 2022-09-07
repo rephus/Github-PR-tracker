0cd894a41bf805176b179747af6cf58c074dc8b9
Rephus <rephus@gmail.com>
2022-09-07 20:27:22 +0200

    Update

--- public/js/github.js
+++ public/js/github.js
@@ -1,6 +1,7 @@
 var lastUpdate = new Date();
 var myUser = "";
 
+let myPrs = [];
 
 $(document).ready(function(){
 
@@ -12,14 +13,21 @@ $(document).ready(function(){
         else myUser = json.response.name;
     }
   })
+
   $("#search-button").click(function(){
     //Disabled updated notifications
     lastUpdate = new Date();  
     search();
+    report(); 
   });
 
   setInterval(search, 60*5*1000); // reload every 5 minutes
 })
+
+function changeFavicon(favicon) {
+  const refreshFav = `${favicon}?v=${(new Date).getTime()}`
+  $("#favicon").attr('href', refreshFav)
+}
 
 function notify(msg) {
   var title = "Github PR notification"; 
@@ -77,7 +85,7 @@ function search(){
           console.error("Unable to get issues ", json.error);
           return
         }
-          var myPrs = [];
+        myPrs = [];
           var otherPrs = [];
 
           var getGroup = function(issues, groupName){
@@ -121,7 +129,115 @@ function search(){
 
           $("#my-prs").html("<b>My PRS</b>").append(myPrsRendered);
           $("#other-prs").html("<b>Other PRs</b>").append(otherPrsRendered);
-          if (nUpdated > 0 ) notify(nUpdated + " issues updated");
+          if (nUpdated > 0 ) {
+            notify(nUpdated + " issues updated");
+            changeFavicon('favicon-red.png')
+
+          } else {
+            changeFavicon('favicon-green.png')
+          }
       }
     })
   }
+  
+
+
+function report(){
+  const excludedPrs = [2085] // donut
+  var token = $("#token").val();
+
+  if (!token) alert("Search requires a private token to work");
+
+
+  const report = $("#today-report")
+  report.html("I'm off :wave:</br>")
+
+  report.append("In progress:</br>")
+  myPrs.flatMap(a => a).filter(i => !excludedPrs.includes(i.number)).forEach( (issue) => {
+
+    report.append(`:white_square: [${issue.title}](${issue.html_url})</br>`);
+  })
+
+  var url = "issues-search?token="+token+"&search=is:pr is:closed repo:lightdash/lightdash author:rephus";
+
+  $.ajax({
+    url: url,
+    success: function(json){
+      if (json.error) {
+        console.error("Unable to get issues ", json.error);
+        return
+      }
+      const yesterday = new Date(new Date().getTime() - 24*60*60*1000);
+      const yesterdayEndDay = new Date(yesterday.toLocaleDateString() + " 17:20")
+      const closedToday = json.items.filter(item => new Date(item.closed_at) > yesterdayEndDay)
+
+       report.append("Closed:</br>")
+      closedToday.forEach( (issue) => {
+        report.append(`:white_check_mark: [${issue.title}](${issue.html_url})</br>`);
+      })
+    }
+  })
+}
+
+
+/*
+function groupSearch(search, group){
+  var token = $("#token").val();
+
+  if (!token) alert("Search requires a private token to work");
+  if (!search) alert("Search can't be empty, eg: is:open");
+  if (!token || !search) return;
+
+  var url = "issues-search?token="+token+"&search="+search;
+  var nUpdated = 0;
+
+  $.ajax({
+    url: url,
+    success: function(json){
+      if (json.error) {
+        console.error("Unable to get issues ", json.error);
+        return
+      }
+        var prs = [];
+
+        var getGroup = function(issues, groupName){
+          for (var i = 0; i <issues.length; i++){
+            if (issues[i][0].title === groupName) return i;
+          }
+          return -1;
+        }
+        var addIssue = function(issues, group, issue){
+          var groupId = getGroup(issues, group);
+          if (groupId < 0) issues.push([issue]);
+          else issues[groupId].push(issue);
+        }
+
+        github= json;
+
+        for (var i=0 ; i < json.items.length; i++){
+          var issue = json.items[i];
+          var group = issue.title;
+
+          var rep_url = issue.repository_url.split("/");
+          var repository = rep_url[rep_url.length -1];
+          issue.repository = repository;
+          //Replace repository_url from api.github to github.com
+          issue.repository_url = issue.repository_url.replace('api.', '').replace('repos/', '');
+
+          issue.updated = new Date(issue.updated_at) > lastUpdate;
+          if (issue.updated) nUpdated++;
+
+          
+            addIssue(prs, group, issue);
+          
+        }
+
+        var template = $('#file-template').html();
+        Mustache.parse(template);
+        var rendered = Mustache.render(template, prs);
+
+        $(group).html(rendered);
+        if (nUpdated > 0 ) notify(nUpdated + " issues updated");
+    }
+  })
+}*/
